36bc41b063019775a6f5bafd039e0bb22864c06e
roggc <roggc9@gmail.com>
2024-07-05 15:24:05 +0200

    space

--- src/comp1/comp1-action.js
+++ src/comp1/comp1-action.js
@@ -5,5 +5,6 @@ export default function comp1({ name }) {
   const messagePromise = new Promise((res) =>
     setTimeout(() => res("hello x " + name), 1000)
   );
+
   return <Comp1 greetingPromise={messagePromise} />;
 }
